28d4c6602dc277cb6d9a0e1dd5f98265bd1ca3b1
Tommy2499 <tommydougherty99@gmail.com>
2025-01-06 11:30:00 -0800

    created basic menu bars

---
+++ src/BottomMenu.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css'
+
+function BottomMenu() {
+
+    return (
+        <div className='btm-menu-container'>Bottom Menu Here
+        </div>
+    );
+
+}
+
+export default BottomMenu;
---
+++ src/TopMenu.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css'
+
+function TopMenu() {
+
+    return (
+        <div className='top-menu-container'>Top Menu Here
+        </div>
+    );
+
+}
+
+export default TopMenu;
