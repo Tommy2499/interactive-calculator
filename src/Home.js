48dfcbefdb626486621b62c37eb497dd09d11299
Tommy2499 <tommydougherty99@gmail.com>
2025-01-06 09:54:47 -0800

    created grid system for calculator links

--- src/Home.js
+++ src/Home.js
@@ -1,17 +1,60 @@
 import React from 'react';
-import { Button, Container } from 'react-bootstrap';
+import { Button, Container, Row, Col } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
+import './Home.css';
 
 function Home() {
   return (
     <div className="home">
       <h1>Welcome to My Calculator App</h1>
       <p>Click the button below to access the calculator:</p>
-      <Link to="/calculator">
-        <Button style={{ padding: '10px 20px', fontSize: '1.2rem', cursor: 'pointer' }}>
-          Go to Calculator
-        </Button>
-      </Link>
+      <div className='link-container'>
+        <Link to="/calculator">
+            <Button className='calc-link'>
+                Go to Calculator
+            </Button>
+        </Link>
+        <Link to="/calculator">
+            <Button className='calc-link'>
+                Go to Calculator
+            </Button>
+        </Link>
+        <Link to="/calculator">
+            <Button className='calc-link'>
+                Go to Calculator
+            </Button>
+        </Link>
+        <Link to="/calculator">
+            <Button className='calc-link'>
+            Go to Calculator
+            </Button>
+        </Link>
+        <Link to="/calculator">
+            <Button className='calc-link'>
+            Go to Calculator
+            </Button>
+        </Link>
+        <Link to="/calculator">
+            <Button className='calc-link'>
+            Go to Calculator
+            </Button>
+        </Link>
+        <Link to="/calculator">
+            <Button className='calc-link'>
+            Go to Calculator
+            </Button>
+        </Link>
+        <Link to="/calculator">
+            <Button className='calc-link'>
+            Go to Calculator
+            </Button>
+        </Link>
+        <Link to="/calculator">
+            <Button className='calc-link'>
+            Go to Calculator
+            </Button>
+        </Link>
+      </div>
     </div>
   );
 }
