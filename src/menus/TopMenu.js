2c66d8c63489476d56b034bf11f277e8dce958b6
Thomas Dougherty <tmdougherty@wisc.edu>
2025-03-06 13:02:24 -0600

    made calculators on top menu a dropdown menu to navigate to calculators

--- src/menus/TopMenu.js
+++ src/menus/TopMenu.js
@@ -1,10 +1,8 @@
 import React from 'react';
-import './TopMenu.css'
+import './TopMenu.css';
 import { Link } from 'react-router-dom';
-import basename from '../config.js';
 
 function TopMenu() {
-
     return (
         <div className='top-overall-container'>
             <div className='top-menu-container'>
@@ -12,14 +10,22 @@ function TopMenu() {
                     <Link to={`/`} className="nav-link">Calc It</Link>
                 </div>
                 <div className='right-top-menu'>
-                    <div>Calculators</div>
+                    <div className='dropdown'>
+                        <div className='dropdown-title'>
+                            <Link to={`/`} className='nav-link'>Calculators</Link>
+                        </div>
+                        <div className='dropdown-content'>
+                            <Link to={`/basic-calculator`} className="dropdown-link" id="first-dropdown">Basic Calculator</Link>
+                            <Link to={`/date-difference`} className="dropdown-link">Date Difference</Link>
+                            <Link to={`/trackulator`} className="dropdown-link">Trackulator</Link>
+                        </div>
+                    </div>
                     <div>About</div>
                 </div>
             </div>
             <div className='border-line'/>
         </div>
     );
-
 }
 
 export default TopMenu;
